0b013df6dcccf8674f655a52535830c20e2eb48e
yum1234 <ryanmoneyryan@yahoo.com>
2023-01-25 11:36:05 -0800

    worked on card and hand class

--- client/card/Hand.java
+++ client/card/Hand.java
@@ -1,6 +1,21 @@
 package Client.card;
-
+//import java.lang.Math;
+import java.util.*;
 
 public class Hand {
-    
+    ArrayList<Card> cards = new ArrayList<Card>();
+    Random ran = new Random();
+
+
+    public Hand(int count) {
+        for(int i = 0; i < count; i++) {
+            cards.add(new Card(ran.nextInt(4)+1, ran.nextInt(9)+1));
+        }
+    }
+
+
+    public void addToDeck(int index, Deck deck) {
+        deck.cards.add(cards.get(index));
+        cards.remove(index);
+    }
 }
