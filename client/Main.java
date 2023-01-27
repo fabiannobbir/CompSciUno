51ef54d040caf25d95784033a7b4464e4e205845
yum1234 <ryanmoneyryan@yahoo.com>
2023-01-27 11:35:59 -0800

    worked more on main

--- client/Main.java
+++ client/Main.java
@@ -4,17 +4,46 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 class Main{
-    public static ArrayList<Player> players;
-    public static Scanner reader;
+    public static ArrayList<Player> players = new ArrayList<Player>();
+    public static Scanner reader = new Scanner(System.in);
+    public static Deck deck = new Deck();
+    public static boolean gameRun = true;
     public static void main(String[] args) {
-        setup();
+        setup(1);
+
+        while(gameRun) {
+
+        }
     }
 
-    public static void setup() {
-        players = new ArrayList<Player>();
-        players.add(new Player("ryan dood", "password", 7));
-        reader = new Scanner(System.in);
-        System.out.println(players.get(0));
+
+
+
+    public static void setup(int player_count) {
+        String username;
+        String id;
+        int numCards;
+
+        for(int i = 0; i < player_count; i++) {
+            System.out.println("Username: ");
+            username = reader.nextLine();
+            System.out.println("id: ");
+            id = reader.nextLine();
+            System.out.println("numCards: ");
+            numCards = reader.nextInt();
+
+            players.add(new Player(username, id, numCards));
+        }
+        
+        
+        //System.out.println(players.get(0));
     }
+
+
+    public static void play() {
+        
+    }
+
+
 }
 
