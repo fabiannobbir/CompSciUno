95269da7254116ad8ca33329ee888497dee1d7ca
Blake Benfield <Blakefbenfield101@gmail.com>
2023-02-02 00:19:58 -0800

    Client side implementation

--- client/card/Player.java
+++ client/card/Player.java
@@ -2,23 +2,24 @@ package client.card;
 
 import client.card.Hand;
 
+import java.io.Serializable;
 import java.util.Random;
 
-public class Player {
+public class Player implements Serializable {
     private String username;
     private final long id;
     private Hand hand;
     
     public Player(String username, int numCards) {
         this.username = username;
-        this.id = 5;
+        this.id = 123412341324L;//genID();
         hand = new Hand(numCards);
     }
 
-    // private long genID(){
-    //     Random random = new Random();
-    //     return random.nextLong((long) (1E18), (long) (9E18));
-    // }
+    private long genID(){
+        Random random = new Random();
+        return (long)((1E18) + (9E18 - 1E18) * random.nextDouble());
+    }
 
     public String toString() {
         String print = "Player " + username + " has cards: ";
@@ -31,9 +32,9 @@ public class Player {
         return username;
     }
 
-    // public long getId() {
-    //     return id;
-    // }
+    public long getId() {
+        return id;
+    }
 
     public Hand getHand() {
         return hand;
---
+++ client/client/ConnectionHandler.java
@@ -0,0 +1,119 @@
+package client.client;
+
+import client.card.Player;
+import server.Game;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
+
+public class ConnectionHandler {
+    private final Player player;
+    private long gameID;
+    private final String serverIP = "http://127.0.0.1:9090/";
+    public ConnectionHandler(Player player) {
+        this.player =  player;
+        System.out.println("Client initialized with id " + player.getId());
+        this.gameID = 2308216752978325504L;
+    }
+
+
+
+    public HashMap<Long, Game> getGames() throws IOException, ClassNotFoundException{
+        URL url = new URL(serverIP + "games");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("GET");
+        connection.setUseCaches(false);
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(connection.getInputStream());
+        HashMap<Long, Game> games = (HashMap<Long, Game>) objectInputStream.readObject();
+        objectInputStream.close();
+        connection.disconnect();
+
+        return games;
+    }
+
+    public Game update() throws IOException, ClassNotFoundException {
+        URL url = new URL(serverIP + "update");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("GET");
+        connection.setUseCaches(false);
+        connection.setRequestProperty("Client-ID", String.valueOf(player.getId()));
+        connection.setRequestProperty("Game-ID", String.valueOf(gameID));
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(connection.getInputStream());
+        Game game = (Game) objectInputStream.readObject();
+        objectInputStream.close();
+        connection.disconnect();
+
+        return game;
+    }
+
+    public void makeGame(Game game) throws IOException, ClassNotFoundException {
+
+        game.addPlayer(player);
+
+        URL url = new URL(serverIP + "game");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setUseCaches(false);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(connection.getOutputStream());
+        objectOutputStream.writeObject(game);
+
+        InputStream inputStream = connection.getInputStream();
+        byte[] gameIDBytes = new byte[connection.getContentLength()];
+        inputStream.read(gameIDBytes);
+        gameID = Long.parseLong
+                (
+                        new String(gameIDBytes, StandardCharsets.UTF_8)
+                );
+        connection.disconnect();
+
+        //TODO remove
+        System.out.println(gameID);
+    }
+
+    public void takeTurn(int cardIndex) throws IOException {
+        URL url = new URL(serverIP + "turn");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setUseCaches(false);
+        connection.setRequestProperty("Client-ID", String.valueOf(player.getId()));
+        connection.setRequestProperty("Game-ID", String.valueOf(gameID));
+        connection.setRequestProperty("Card-Index", String.valueOf(cardIndex));
+        connection.disconnect();
+    }
+
+    public void joinGame(long id) throws IOException {
+        URL url = new URL(serverIP + "join");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setUseCaches(false);
+        connection.setRequestProperty("Game-ID", String.valueOf(id));
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(connection.getOutputStream());
+        objectOutputStream.writeObject(player);
+        connection.getResponseCode(); //For some reason this is needed
+        connection.disconnect();
+    }
+
+
+    public static int sizeof(Object obj) throws IOException {
+
+        ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteOutputStream);
+
+        objectOutputStream.writeObject(obj);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+
+        return byteOutputStream.toByteArray().length;
+    }
+}
