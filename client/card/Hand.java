5be24cf78caee009ce8387d5b78e6b1ac7be6be2
yum1234 <ryanmoneyryan@yahoo.com>
2023-02-04 18:17:41 -0800

    Working ASCII code implmentation

--- client/card/Hand.java
+++ client/card/Hand.java
@@ -21,36 +21,127 @@ public class Hand implements Serializable {
     }
 
     public String toString() {
-        String print = "";
+        String[] combine = {"", "", "", ""}; //by default, using new String[4] will contain a null. by creating the string object, it won't print out null :)
+        String print = "\n";
         Card card;
-        for(int i = 0; i < cards.size(); i++) {
-            card = cards.get(i);
-            print += ascii(card.color, card.value);
-
+        for(int i = 0; i < combine.length; i++) {
+            for(int j = 0; j < cards.size(); j++) {
+                card = cards.get(j);
+                combine[i] += ascii(card.color, card.value)[i] + " ";
+            }
             
+        }
+
+        for(int i = 0; i < combine.length; i++) {
+            print += combine[i] + "\n";
         }
 
         return print;
     }
 
-    public String ascii(String color, int value) {
-        String spaces = "                            ";
-        int size = spaces.length();
+    public String[] ascii(String color, int value) {
+
         if(value == 10) {
-            return "(" + color + ", " + "Skip" + ") ";
+            return special_Cards_Array(color, "Skip");
         } else if (value == 11) {
-            return "(" + color + ", " + "Reverse" + ") ";
+            return special_Cards_Array(color, "R");
         } else if (value == 12) {
-            return "(" + color + ", " + "+2" + ") ";
+            return special_Cards_Array(color, "+2");
         } else if (value == 13) {
-            return "(" + color + ", " + "+4" + ") ";
+            return special_Cards_Array(color, "+4");
         } else if (value == 14) {
-            return "(" + "Wild" + ") ";
+            return special_Cards_Array(color, "Wild");
         } else{
-            return regular_cards(color, value) + "\n";
+            return regular_Cards_Array(color, value);
         }
 
+
+        //return regular_Cards_Array(color, value);
+
     }
+
+    public String[] regular_Cards_Array(String color, int value) {
+        String[] print = {"", "", "", ""}; //by default, using new String[4] will contain a null. by creating the string object, it won't print out null :)
+        if(color.equals("Red")) {
+            print[0] += "[RED    " + value + "]";
+            print[1] += "[        ]";
+            print[2] += "[        ]";
+            print[3] += "[" + value + "    RED]";
+        }
+        if(color.equals("Blue")) {
+            print[0] += "[BLUE   " + value + "]";
+            print[1] += "[        ]";
+            print[2] += "[        ]";
+            print[3] += "[" + value + "   BLUE]";
+        }
+        if(color.equals("Yellow")) {
+            print[0] += "[Yellow " + value + "]";
+            print[1] += "[        ]";
+            print[2] += "[        ]";
+            print[3] += "[" + value + " Yellow]";
+        }
+        if(color.equals("Green")) {
+            print[0] += "[Green  " + value + "]";
+            print[1] += "[        ]";
+            print[2] += "[        ]";
+            print[3] += "[" + value + "  Green]";
+        }
+        return print;
+    }
+
+    public String[] special_Cards_Array(String color, String value) {
+        String[] print = {"", "", "", ""}; //by default, using new String[4] will contain a null. by creating the string object, it won't print out null :)
+        String spaces = "        "; // spaces between "[        ]"
+        int size = spaces.length();
+        int between = size-color.length()-value.length();
+        int make_space = 0;
+
+        //this is to help automatically expand the card size if the color and value are too long to fit in one row. Usually this occurs with Yellow + Reverse but is now represented with Yellow + R, so this problem won't occur anymore.
+        if(value.equals("Wild")) {
+            print[0] += "[Wild    ]";
+            print[1] += "[        ]";
+            print[2] += "[        ]";
+            print[3] += "[    Wild]";
+        } else {
+
+            if(between < 0) {
+                make_space = size + between*-1-5;
+            }
+
+
+            print[0] += "[" + color;
+            for(int i = 0; i < between+make_space; i++) {
+                print[0] += " ";
+            }
+            print[0] += value + "]";
+
+            print[1] += "[";
+            for(int i = 0; i < size+make_space; i++) {
+                print[1] += " ";
+            }
+            print[1] += "]";
+
+            print[2] += "[";
+            for(int i = 0; i < size+make_space; i++) {
+                print[2] += " ";
+            }
+            print[2] += "]";
+
+            print[3] += "[" + value;
+            for(int i = 0; i < between+make_space; i++) {
+                print[3] += " ";
+            }
+            print[3] += color + "]";
+        }
+
+        return print;
+    }
+
+
+
+
+
+
 
 
     public String regular_cards(String color, int value) {
